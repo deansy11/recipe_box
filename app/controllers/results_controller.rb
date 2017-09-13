6a617d38ad8f1d5212b3839cafd83adbb987f69a
Emily Deans <emilyldeans@gmail.com>
2017-09-13 15:36:59 -0400

    Added search results view and created pg_search table and generate table migration.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,5 @@
 class ResultsController < ApplicationController
   def index
-    @pg_search_recipes = PgSearch.multisearch(params[:query])
+    @pg_search_recipes = PgSearch.multisearch(params[:query]).page(params[:page]).per(10)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910133909) do
+ActiveRecord::Schema.define(version: 20170913193236) do
 
   create_table "directions", force: :cascade do |t|
     t.text "description"
@@ -21,10 +21,19 @@ ActiveRecord::Schema.define(version: 20170910133909) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "ingredient"
-    t.string "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "recipe_id"
+    t.string "amount"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.integer "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "recipes", force: :cascade do |t|
