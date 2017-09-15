d1496d9ebbd5b9bbcba8b5d57f4e91b86de67a6f
Emily Deans <emilyldeans@gmail.com>
2017-09-15 15:31:17 -0400

    Removed ingredient from recipe schema.

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,43 +1,4 @@
 class DirectionsController < ApplicationController
   def index
   end
-
-  def new
-    @recipe = Recipe.find_by(id: params[:recipe_id])
-    @direction = Direction.new
-  end
-
-  def create
-    @recipe = Recipe.find_by(id: params[:recipe_id])
-    @direction = Direction.new
-    @direction.recipe = @recipe
-
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to recipes_url }
-        format.json { render :show, status: :created, title: @ingredient }
-      else
-        format.html { render :new }
-        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def edit
-
-  end
-
-  def destroy
-    @direction.destroy
-    respond_to do |format|
-        format.html { redirect_to recipes_url, notice: 'Step was successfully destroyed.' }
-        format.json { head :no_content }
-  end
-
-  private
-
-    def ingredient_params
-      params.require(:direction).permit(:description)
-    end
-
 end
---
+++ db/migrate/20170915193100_remove_ingredients_from_recipe.rb
@@ -0,0 +1,5 @@
+class RemoveIngredientsFromRecipe < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :recipes, :ingredients, :string
+  end
+end
