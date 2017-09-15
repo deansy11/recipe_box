ceb564ba3844ce2493018587e966b08cdd1d0a11
Emily Deans <emilyldeans@gmail.com>
2017-09-15 14:37:06 -0400

    Removed faulty tests.

--- test/controllers/directions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DirectionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get directions_index_url
-    assert_response :success
-  end
-
-end
